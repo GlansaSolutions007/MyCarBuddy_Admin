2971cf22b24a639f2a0263d75e01a73b330250fb
poornendraparouha <poornendraparouha@gmail.com>
2026-07-23 16:08:34 +0530

    dashboard updated wih repeated booking

--- src/components/child/UnitCountSeven.jsx
+++ src/components/child/UnitCountSeven.jsx
@@ -153,7 +153,7 @@ const UnitCountSeven = () => {
                 </p>
               </div>
             </div> */}
-             <div className='col-xxl-4 col-xl-4 col-sm-6'>
+             <div className='col-xxl-3 col-xl-3 col-sm-6'>
                 <div className='px-20 py-16 shadow-none radius-8 h-100 gradient-deep-2 left-line line-bg-lilac position-relative overflow-hidden'>
                   <div className='d-flex flex-wrap align-items-center justify-content-between gap-1 mb-8'>
                     <div>
@@ -168,12 +168,12 @@ const UnitCountSeven = () => {
                           : (data.TotalBookings || 0)}
                       </h6>
                     </div>
-                    <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-lilac-200 text-lilac-600'>
+                    {/* <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-lilac-200 text-lilac-600'>
                       <i className="ri-file-list-3-fill"/>
-                    </span>
+                    </span> */}
                   </div>
                   {/* 👇 FIXED POSITION - no height increase */}
-                  {department !== "Support Department" || role === "Admin" ? (
+                  {/* {department !== "Support Department" || role === "Admin" ? (
                     <span
                       style={{
                         position: "absolute",
@@ -186,11 +186,11 @@ const UnitCountSeven = () => {
                     >
                       Total Leads = {data.TotalLeads || 0}
                     </span>
-                  ) : null}
+                  ) : null} */}
 
                 </div>
               </div>
-              <div className='col-xxl-4 col-xl-4 col-sm-6'>
+              <div className='col-xxl-3 col-xl-3 col-sm-6'>
                 <div className='px-20 py-16 shadow-none radius-8 h-100 gradient-deep-3 left-line line-bg-success position-relative overflow-hidden'>
                   <div className='d-flex flex-wrap align-items-center justify-content-between gap-1 mb-8'>
                     <div>
@@ -201,9 +201,9 @@ const UnitCountSeven = () => {
                         {department === "Support Department" && role !== "Admin" ? resolvedCount : (data.TodayBookings || 0)}
                       </h6>
                     </div>
-                    <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-success-200 text-success-600'>
+                    {/* <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-success-200 text-success-600'>
                       <i className="ri-calendar-check-fill"/>
-                    </span>
+                    </span> */}
                   </div>
                   {/* <p className='text-sm mb-0'>
                   <span className='bg-danger-focus px-1 rounded-2 fw-medium text-danger-main text-sm'>
@@ -213,7 +213,7 @@ const UnitCountSeven = () => {
                 </p> */}
                 </div>
               </div>
-              <div className='col-xxl-4 col-xl-4 col-sm-6'>
+              <div className='col-xxl-3 col-xl-3 col-sm-6'>
                 <div className='px-20 py-16 shadow-none radius-8 h-100 gradient-deep-4 left-line line-bg-warning position-relative overflow-hidden'>
                   <div className='d-flex flex-wrap align-items-center justify-content-between gap-1 mb-8'>
                     <div>
@@ -226,16 +226,37 @@ const UnitCountSeven = () => {
                         </h6>
                       {/* </Link> */}
                     </div>
-                    <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-warning-focus text-warning-600'>
+                    {/* <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-warning-focus text-warning-600'>
                       <i className="ri-checkbox-circle-fill"/>
-                    </span>
+                    </span> */}
                   </div>
+                  
                   {/* <p className='text-sm mb-0'>
                   <span className='bg-success-focus px-1 rounded-2 fw-medium text-success-main text-sm'>
                     <i className='ri-arrow-right-up-line' /> 60%
                   </span>{" "}
                   From last month{" "}
                 </p> */}
+                </div>
+              </div>
+
+              <div className='col-xxl-3 col-xl-3 col-sm-6'>
+                <div className='px-20 py-16 shadow-none radius-8 h-100 gradient-deep-1 left-line line-bg-info position-relative overflow-hidden'>
+                  <div className='d-flex flex-wrap align-items-center justify-content-between gap-1 mb-8'>
+                    <div>
+                      {/* <Link to="/refunds"> */}
+                        <span className='mb-2 fw-medium text-secondary-light text-md'>
+                          {department === "Support Department" && role !== "Admin" ? "" : "Repeated Bookings"}
+                        </span>
+                        <h6 className='fw-semibold mb-1'>
+                          {department === "Support Department" && role !== "Admin" ? "" : (data.RepeatedBookings || "0") }
+                        </h6>
+                      {/* </Link> */}
+                    </div>
+                    {/* <span className='w-44-px h-44-px radius-8 d-inline-flex justify-content-center align-items-center text-2xl mb-12 bg-warning-focus text-warning-600'>
+                      <i className="ri-checkbox-circle-fill"/>
+                    </span> */}
+                  </div>
                 </div>
               </div>
             </div>
